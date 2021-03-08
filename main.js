9946e998bbdfbe6c2eb3e3e907709651ffdbcd54
priyanshudangare <79207190+priyanshudangare@users.noreply.github.com>
2021-03-08 16:57:13 +0530

    Plasma Effect

--- main.js
+++ main.js
@@ -1,24 +1,4 @@
-var theme = 1;
 var menu = 0;
-
-function toggleTheme() {
-	var r = document.querySelector(':root');
-	if (theme == 0) {
-		theme = 1;
-		r.style.setProperty('--bg-color', '#1f1f1f');
-		r.style.setProperty('--fg-color', 'white');
-		r.style.setProperty('--fg-color2', '#606060');
-		document.getElementById("mobilemenuimg").src = "menudark.svg";
-	} else {
-		theme = 0;
-		r.style.setProperty('--bg-color', 'white');
-		r.style.setProperty('--fg-color', 'black');
-		r.style.setProperty('--fg-color2', '#404040');
-		document.getElementById("mobilemenuimg").src = "menulight.svg";
-	}
-}
-
-toggleTheme();
 
 function toggleMenu() {
 	var nav = document.getElementById("nav");
--- plasma.py
+++ plasma.py
@@ -5,7 +5,7 @@ from math import sqrt
 
 print("--------------: Plasma Effect Generator :--------------")
 
-size = 2048, 2048
+size = 256, 256
 
 myImage = Image.new('RGB',size,color=(255,0,0))
 
@@ -13,9 +13,9 @@ pi = 3.14159
 
 for x in range(size[0]): 
 	for y in range(size[1]):
-		scale = 1/5
-		sx = x/size[0]*360
-		sy = y/size[1]*360
+		scale = 1/20
+		sx = x
+		sy = y
 		v = 0
 		v += sin(sx*scale)
 		v += sin((sx+sy)*0.5*scale)
@@ -23,9 +23,14 @@ for x in range(size[0]):
 		r = sin(v*pi)
 		g = sin(v*pi+2*pi/3)
 		b = sin(v*pi+4*pi/3)
+		#r = v
+		#g = r
+		#b = r
 		r = 127.5+r*127.5
 		g = 127.5+g*127.5
 		b = 127.5+b*127.5
 		myImage.putpixel((x,y), (int(r),int(g),int(b)))
 
 myImage.save("plasma.png")
+
+myImage.show()
